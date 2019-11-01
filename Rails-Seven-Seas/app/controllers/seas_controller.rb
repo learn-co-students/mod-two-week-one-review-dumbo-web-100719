d68435f02347420d0643424050fb803c2fa889d5
github username <github email address>
2019-11-01 12:01:15 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,51 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+    @seas = Sea.all
+  end
+
+  def new
+  end
+
+  def create
+      @sea = Sea.new(sea_params)
+     
+      @sea.save
+      redirect_to @sea
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+   
+    if @sea.update(sea_params)
+      redirect_to @sea
+    else
+      render 'edit'
+    end
+  end
+  
+    def destroy
+  
+      @sea = Sea.find(params[:id])
+      @sea.destroy
+      redirect_to seas_path
+    end
+  
+  
+  
+  
+  
+
+
+
 
 
   private
